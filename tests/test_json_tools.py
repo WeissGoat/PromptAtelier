33e9d40645040ab33c315f8192a0196aae466e0c
whitesheep <260886429@qq.com>
2026-05-25 07:33:13 +0800

    Cover nested JSON display truncation

--- tests/test_json_tools.py
+++ tests/test_json_tools.py
@@ -57,6 +57,77 @@ class JsonToolsTest(unittest.TestCase):
 
         self.assertEqual(sanitized, data)
 
+    def test_sanitize_json_for_display_truncates_deep_nested_image_fields(self):
+        image_text = "i" * 100
+        prompt_text = "p" * 40
+        data = {
+            "render_request": {
+                "params": {
+                    "reference_image_multiple": [image_text],
+                    "workflow_json": {
+                        "12": {
+                            "inputs": {
+                                "image": image_text,
+                                "mask": image_text,
+                                "text": prompt_text,
+                            }
+                        }
+                    },
+                }
+            },
+            "generation_result": {
+                "request_body": {
+                    "parameters": {
+                        "director_reference_images": [image_text],
+                    }
+                }
+            },
+        }
+
+        sanitized = sanitize_json_for_display(
+            data,
+            max_image_string_length=10,
+            max_string_length=50,
+        )
+
+        self.assertEqual(
+            sanitized["render_request"]["params"]["reference_image_multiple"][0],
+            "iiiiiiiiii...(truncated, chars=100)",
+        )
+        workflow_inputs = sanitized["render_request"]["params"]["workflow_json"]["12"]["inputs"]
+        self.assertEqual(workflow_inputs["image"], "iiiiiiiiii...(truncated, chars=100)")
+        self.assertEqual(workflow_inputs["mask"], "iiiiiiiiii...(truncated, chars=100)")
+        self.assertEqual(workflow_inputs["text"], prompt_text)
+        self.assertEqual(
+            sanitized["generation_result"]["request_body"]["parameters"][
+                "director_reference_images"
+            ][0],
+            "iiiiiiiiii...(truncated, chars=100)",
+        )
+
+    def test_sanitize_json_for_display_keeps_non_image_prompt_until_general_limit(self):
+        prompt_text = "p" * 40
+        data = {
+            "request_body": {
+                "parameters": {
+                    "prompt": prompt_text,
+                    "negative_prompt": prompt_text,
+                }
+            }
+        }
+
+        sanitized = sanitize_json_for_display(
+            data,
+            max_image_string_length=10,
+            max_string_length=50,
+        )
+
+        self.assertEqual(sanitized["request_body"]["parameters"]["prompt"], prompt_text)
+        self.assertEqual(
+            sanitized["request_body"]["parameters"]["negative_prompt"],
+            prompt_text,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
