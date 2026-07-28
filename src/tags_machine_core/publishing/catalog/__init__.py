6728ad8e835bdf7a53c840816e2324bc2fcbfb25
whitesheep <260886429@qq.com>
2026-07-29 00:36:03 +0800

    refactor: remove publishing workspace from core

--- src/tags_machine_core/publishing/catalog/__init__.py
+++
@@ -1,3 +0,0 @@
-from .repository import CatalogRepository
-
-__all__ = ["CatalogRepository"]
--- src/tags_machine_core/publishing/inputs/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base import InputAdapterRegistry, InputContext, default_input_registry
-
-__all__ = ["InputAdapterRegistry", "InputContext", "default_input_registry"]
